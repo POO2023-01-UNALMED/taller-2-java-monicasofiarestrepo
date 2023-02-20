81c41b9c537baad29dfe98e14098a843210e5406
MSRestreLeon <monicarestrepoleon@gmail.com>
2023-02-19 21:56:15 -0500

    deleted something

---
+++ src/test/java/test/Asiento.java
@@ -0,0 +1,7 @@
+package test;
+
+public class ClaseAsiento {
+    String color;
+    int precio; 
+    
+}
---
+++ src/test/java/test/Auto.java
@@ -0,0 +1,13 @@
+package test;
+
+import java.lang.reflect.Array;
+
+public class ClaseAuto {
+    String modelo;
+    int precio; 
+    Array asientos; 
+    String marca; 
+    Motor motor; 
+    int registro;
+    int cantidadCreados; 
+}
